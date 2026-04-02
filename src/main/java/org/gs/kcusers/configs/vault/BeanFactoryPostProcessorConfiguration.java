df1ee19748077b7f9f51642c63440805d33ce3c2
Eugene Sokolov <greatsokol@gmail.com>
2026-04-02 08:11:00 +0300

    Arrays.stream in BeanFactoryPostProcessorConfiguration.java

--- src/main/java/org/gs/kcusers/configs/vault/BeanFactoryPostProcessorConfiguration.java
+++ src/main/java/org/gs/kcusers/configs/vault/BeanFactoryPostProcessorConfiguration.java
@@ -5,15 +5,14 @@ import org.springframework.boot.autoconfigure.web.ServerProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.stream.Stream;
+import java.util.Arrays;
 
 @Configuration
 public class BeanFactoryPostProcessorConfiguration {
     @Bean
     public static BeanFactoryPostProcessor dependsOnPostProcessor() {
         return bf -> {
-            String[] jpa = bf.getBeanNamesForType(ServerProperties.class);
-            Stream.of(jpa)
+            Arrays.stream(bf.getBeanNamesForType(ServerProperties.class))
                     .map(bf::getBeanDefinition)
                     .forEach(it -> it.setDependsOn("VaultDataImport"));
         };
